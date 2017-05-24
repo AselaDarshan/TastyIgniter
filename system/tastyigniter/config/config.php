a2f007fae2168839963b0cb5c06dad2b8c00a6fa
AselaDarshan <aseladarshan@gmail.com>
2017-05-24 14:43:42 +0530

    Add API

--- system/tastyigniter/config/config.php
+++ system/tastyigniter/config/config.php
@@ -316,7 +316,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = "N4Vw2QBa1w5e1X58zsyCZawrTVIIy944";
 
 /*
 |--------------------------------------------------------------------------
